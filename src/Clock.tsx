76af7a12c71e08c846dfadebaa610914934b2d96
Wildes Piva <wildes.piva@nappsolutions.com>
2023-11-23 13:43:56 -0300

    feat: no sleep

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
+import NoSleep from "@uriopass/nosleep.js";
 import "./clock.css";
+
+const noSleep = new NoSleep();
 
 export default function Clock() {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
 
   useEffect(() => {
     const interval = setInterval(updateClockTime);
+    noSleep.enable();
     return () => {
       clearInterval(interval);
+      noSleep.disable();
     };
   }, []);
 
